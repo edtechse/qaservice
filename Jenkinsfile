e9b43f713c7bbbdf99301fc1ba0fa3af9edbb3d7
Niya Aniyan <niya.aniyan@u.nus.edu>
2021-09-30 11:58:12 +0530

    Build pipeline changes+ AWS creds

---
+++ Jenkinsfile
@@ -0,0 +1,45 @@
+pipeline {
+    agent any
+    environment {
+        AWS_ACCOUNT_ID="162387011843"
+        AWS_DEFAULT_REGION="ap-southeast-1"
+        IMAGE_REPO_NAME="qaservice-repo"
+        IMAGE_TAG="latest"
+        REPOSITORY_URI = "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}"
+    }
+  
+    stages {   
+      stage('Logging into AWS ECR') {
+            steps {
+                script {
+                sh 'docker login --username AWS -p $(aws ecr get-login-password --region ap-southeast-1) 162387011843.dkr.ecr.ap-southeast-1.amazonaws.com'  
+                }
+                
+            }
+        }
+        stage('Cloning Git') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '', url: 'https://github.com/edtechse/qaservice.git']]])
+            }
+        }
+ 
+    // Building Docker images
+    stage('Building image') {
+      steps{
+        script {
+          dockerImage = docker.build "${IMAGE_REPO_NAME}:${IMAGE_TAG}"
+        }
+      }
+    }
+  
+    // Uploading Docker images into AWS ECR
+    stage('Pushing to ECR') {
+     steps{
+         script {
+                sh "docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${REPOSITORY_URI}:$IMAGE_TAG"
+                sh "docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}"
+         }
+        }
+      }
+    }
+}
--- src/main/java/com/edtech/qaservice/config/AWSConfig.java
+++ src/main/java/com/edtech/qaservice/config/AWSConfig.java
@@ -1,21 +1,14 @@
 package com.edtech.qaservice.config;
 
-import com.amazon.dax.client.dynamodbv2.ClientConfig;
-import com.amazon.dax.client.dynamodbv2.ClusterDaxAsyncClient;
-import com.amazon.dax.client.dynamodbv2.ClusterDaxClient;
+import com.amazon.dax.client.dynamodbv2.AmazonDaxClientBuilder;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazon.dax.client.dynamodbv2.AmazonDaxClientBuilder;
-
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,12 +16,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AWSConfig {
-
-    @Value("${amazon.access.key}")
-    private String amazonAWSAccessKey;
-
-    @Value("${amazon.access.secret-key}")
-    private String amazonAWSSecretKey;
 
     @Value("${amazon.region}")
     private String awsRegion;
@@ -68,7 +55,7 @@ public class AWSConfig {
 
     @Bean
     public AWSCredentials amazonAWSCredentials() {
-        return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
+        return  new DefaultAWSCredentialsProviderChain().getCredentials();
     }
 
 }
